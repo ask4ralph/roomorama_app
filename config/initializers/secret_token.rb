c979f55d70fa3e9d3ffe88a5936300e184780fc7
Raphael Okochu <ROkochu@Interswitchng.com>
2014-11-07 12:13:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoomoramaApp::Application.config.secret_token = 'bde62b9a27a70acf54b6423913c5058aca2153e90fd9f4321d36d5d09c2a1da5cbc246de2a1c50a7ba23424f85a7b4ce58fa83d7886157df23671cca3bd18fce'
